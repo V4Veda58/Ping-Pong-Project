109253569349ed768d3e36ed2029fff6e43e0f82
V4Veda58 <85873657+V4Veda58@users.noreply.github.com>
2022-05-16 12:50:50 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -22,11 +22,19 @@ var ball = {
 }
 
 function setup(){
-  var canvas =  createCanvas(700,600);
+canvas =  createCanvas(700,600);
+canvas.parent('canvas');
+video = createCapture(VIDEO);
+video.hide();
+poseNet = ml5.poseNet(video, modelLoaded);
+}
+function modelLoaded()
+{
+  console.log('Model Loaded!');
 }
 
-
 function draw(){
+image(video, 0, 0, 700, 600);
 
  background(0); 
 
